e7d1a3c82d04f74f0a5f4ab72eb62337a1218df7
ClaudiaHdezPerez <121737030+ClaudiaHdezPerez@users.noreply.github.com>
2025-06-08 16:39:14 -0400

    fix/string to token array

--- regex_interpreter/match.h
+++ regex_interpreter/match.h
@@ -2,11 +2,12 @@
 #define MATCH_H
 
 #include "dfa.h"
+#include "token.h"
 
 typedef struct {
     int matched;
-    char** tokens;
-    int matches;
+    Lexer_Token* tokens;
+    int count;
 } String_Match;
 
 String_Match* match(DFA* dfa, char* str);
---
+++ regex_interpreter/token.h
@@ -0,0 +1,9 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef struct {
+    char lexeme[256];
+    char token[256];
+} Lexer_Token;
+
+#endif
